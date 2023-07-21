061ea617f650ade1a600a5ed02faab4a1514bff0
Jie945 <124592607+Jie945@users.noreply.github.com>
2023-07-21 20:59:08 +0800

    Delete knn.m

--- knn.m
+++
@@ -1,9 +0,0 @@
- function [knn_network] = knn(P,k)
-    [~,n] = size(P);
-    knn_network = zeros(n, n);
-    [sort_network,idx]=sort(P,2,'descend');
-    for i = 1 : n
-       knn_network(i,idx(i,1:k))=sort_network(i,1:k);
-    end
- 
-end
